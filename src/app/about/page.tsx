310d054b5f2f910a53a153902b1092c7665fa7af
Chethan <Chethanprakash720@gmail.com>
2024-06-02 14:31:39 +0530

    routings added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+    return (
+      <div className="p-6 mt-20">
+        <h1 className="text-3xl font-bold mb-4">About me:</h1>
+        <hr className="bg-gray-0 my-4 opacity-25" />
+        <div className="max-w-md">
+          <p className="text-base leading-loose text-gray-200">
+            Hey there! I'm Chethan. <br />
+            <span className="leading-normal">An Aspiring Full-stack Web Developer with a passion for exploring new technologies and enhancing web performance.</span><br />
+            I'm always on the lookout for ways to simplify and improve the user experience.
+          </p>
+        </div>
+      </div>
+    );
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+    <h1 className="text-3xl font-bold">Chethan</h1>
+    <p className="mt-4">Aspiring Full-Stack Web Developer From India.</p>
+   </div>
+  )
+}
