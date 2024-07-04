22277c2f8b0bc357ac16755c6df54dd299fe66e1
Chethan <chethanprakash720@gmail.com>
2024-07-05 03:06:04 +0530

    ui changes

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -1,3 +1,5 @@
+// src/components/ui/aurora-background.tsx
+"use client";
 import { cn } from "@/utils/cn";
 import React, { ReactNode } from "react";
 
@@ -7,7 +9,7 @@ interface AuroraBackgroundProps extends React.HTMLProps<HTMLDivElement> {
 }
 
 export const AuroraBackground = ({
-  className = "",
+  className,
   children,
   showRadialGradient = true,
   ...props
@@ -25,11 +27,19 @@ export const AuroraBackground = ({
           <div
             className={cn(
               `
-            bg-[linear-gradient(to_right,#4f4f4f,#000000)]
-            animate-aurora
+            [--black-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
+            [--blue-gradient:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--blue-400)_15%,var(--blue-300)_20%,var(--blue-200)_25%,var(--blue-100)_30%)]
+            [background-image:var(--black-gradient),var(--blue-gradient)]
+            [background-size:300%,_200%]
+            [background-position:50%_50%,50%_50%]
+            filter blur-[10px]
+            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--black-gradient),var(--blue-gradient)]
+            after:[background-size:200%,_100%] 
+            after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
             pointer-events-none
-            absolute -inset-[10px] opacity-50`,
-              showRadialGradient ? `mask-[radial-gradient(ellipse_at_100%_0%,black_10%,transparent_70%)]` : '' // Use ternary operator to ensure a string type
+            absolute -inset-[10px] opacity-50 will-change-transform`,
+              showRadialGradient &&
+                `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
             )}
           ></div>
         </div>
--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,7 +1,6 @@
-// src/utils/cn.ts
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
-  }
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
