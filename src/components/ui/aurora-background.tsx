a73e975029358ec530ffb6033df4d5ce809f39b3
Chethan <chethanprakash720@gmail.com>
2024-07-05 02:18:02 +0530

    .json updates

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -1,4 +1,3 @@
-// src/components/ui/aurora-background.tsx
 "use client";
 import { cn } from "@/utils/cn";
 import React, { ReactNode } from "react";
@@ -27,19 +26,12 @@ export const AuroraBackground = ({
           <div
             className={cn(
               `
-            [--black-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
-            [--blue-gradient:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--blue-400)_15%,var(--blue-300)_20%,var(--blue-200)_25%,var(--blue-100)_30%)]
-            [background-image:var(--black-gradient),var(--blue-gradient)]
-            [background-size:300%,_200%]
-            [background-position:50%_50%,50%_50%]
-            filter blur-[10px]
-            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--black-gradient),var(--blue-gradient)]
-            after:[background-size:200%,_100%] 
-            after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
+            bg-[linear-gradient(to_right,#4f4f4f,#000000)]
+            animate-aurora
             pointer-events-none
-            absolute -inset-[10px] opacity-50 will-change-transform`,
+            absolute -inset-[10px] opacity-50`,
               showRadialGradient &&
-                `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
+                `mask-[radial-gradient(ellipse_at_100%_0%,black_10%,transparent_70%)]`
             )}
           ></div>
         </div>
