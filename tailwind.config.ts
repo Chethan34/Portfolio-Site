c595a0b419e904586491b28ac1c39fa7eccb7120
Chethan <chethanprakash720@gmail.com>
2024-07-05 02:22:34 +0530

    dependencies fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        aurora: 'aurora 15s linear infinite',
+      },
+      keyframes: {
+        aurora: {
+          from: { backgroundPosition: '0% 0%' },
+          to: { backgroundPosition: '200% 0%' },
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
