3519d91bbc74883edcbd927afc98e1f4af1ae445
Chethan <chethanprakash720@gmail.com>
2024-07-04 21:42:27 +0530

    ESLINT error fix

--- src/components/homepage/page.tsx
+++ src/components/homepage/page.tsx
@@ -6,7 +6,7 @@ export default function homepage(){
   return (
     <div className="flex flex-col min-h-screen justify-center items-center text-white px-4 sm:px-8">
       <div className="flex flex-col space-y-8 justify-center items-center max-w-4xl w-full mt-[-50px] sm:mt-[-80px]">
-        <div className="grid grid-cols-1 gap-4 items-center mb-1">
+        <div className="grid grid-cols-1 gap-4 items-center mb-4">
           <Image src={Clogo} alt="C logo symbol" width={160} height={130} />
           <div className="flex flex-col justify-center items-center mb-0">
             <h1 className="text-4xl font-bold ml-0">Chethan</h1>
@@ -17,7 +17,7 @@ export default function homepage(){
           Hey, my name is Chethan. A minimalist obsessed with the web world, exploring functionalities and design with curiosity.
         </p>
         <p className='mb-10 text-center'> 
-          I'm always interested in new projects, trying out new technologies, and open source. If you would like to connect, feel free
+          Im always interested in new projects, trying out new technologies, and open source. If you would like to connect, feel free
           to reach out via the links below.
         </p>
         <div className="flex space-x-4 justify-center">
